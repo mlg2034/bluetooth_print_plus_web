a2be284f7044966cb378b45dee3f8585028a1e9d
mlg2034 <ashatkabytaev@gmail.com>
2025-04-02 12:48:04 +0500

    fixed pods

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Askhat\Desktop\bluetooth_print_plus-2.4.5\example"
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.29.2/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/askhatkabtytaev/StudioProjects/bluetooth_print_plus_web/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=/Users/askhatkabtytaev/StudioProjects/bluetooth_print_plus_web/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/askhatkabtytaev/StudioProjects/bluetooth_print_plus_web/example/.dart_tool/package_config.json"
